305e25ca8eb6e7edc5fafb0d6f8c4aae0ff3d600
Jorge <jcarapia83@Gmail.com>
2017-03-22 03:02:34 -0700

    working on header section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Landing from './Landing';
+
 require('../public/main.scss');
 
 class App extends Component {
 
   render() {
     return (
-      <h1>Hello World!</h1> 
+      <div>
+        <Landing />  
+      </div>
+      
     )
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,25 @@ module.exports = {
                 }], 
                 fallback: "style-loader"
             })
+      },
+      {
+    test: /\.(gif|png|jpe?g|svg)$/i,
+    loaders: [
+      'file-loader',
+      {
+        loader: 'image-webpack-loader',
+        query: {
+          progressive: true,
+          optimizationLevel: 7,
+          interlaced: false,
+          pngquant: {
+            quality: '65-90',
+            speed: 4
+          }
+        }
       }
+    ]
+  }
     ]
   },
   plugins: [
